4c43606f7b3507b17dc3c10259c1f14877e8cd1c
GianmarcoMorelli <gianmarco.morelli96@gmail.com>
2023-01-18 13:10:59 +0100

    Commit post test succesfully completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word,num) {
+    let i = 0;
+    let result = "";
 
+    if(num >= 0){
+        for(i = 0; i < num; i++){
+            result = result.concat(word);
+        }
+        return result;
+    }else{
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
