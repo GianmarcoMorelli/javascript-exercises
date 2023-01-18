559c9654e39275c85046197ae591d74bbe5ab1a9
GianmarcoMorelli <gianmarco.morelli96@gmail.com>
2023-01-18 16:12:19 +0100

    Test completed succesfully

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
 const removeFromArray = function() {
-
+    const args = Array.from(arguments);
+    const inputArray = args[0];
+    const undesiredElements = args.slice(1);
+    let len = undesiredElements.length;
+    let i = 0;
+    let tmpIndex = 0;
+    for(i = 0; i < len; i++){
+        if(inputArray.includes(undesiredElements[i])){
+            tmpIndex = inputArray.indexOf(undesiredElements[i]);
+            if(typeof(inputArray[tmpIndex]) == typeof(undesiredElements[i])){
+                inputArray.splice(tmpIndex,1);
+            }
+            
+        }
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
