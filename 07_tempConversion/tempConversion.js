0e23581100c4eede1724ba783af8fc420e3ff6e9
GianmarcoMorelli <gianmarco.morelli96@gmail.com>
2023-01-18 17:45:52 +0100

    Test completed succesfully

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempOne) {
+  let resOne = (tempOne - 32) * (5/9);
+  return parseFloat(resOne.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempTwo) {
+  let resTwo = tempTwo * (9/5) + 32;
+  return parseFloat(resTwo.toFixed(1));
 };
 
 // Do not edit below this line
