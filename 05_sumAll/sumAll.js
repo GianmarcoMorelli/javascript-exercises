d2e964f11f6e99039fc77b0e46c01151757f7846
GianmarcoMorelli <gianmarco.morelli96@gmail.com>
2023-01-18 17:04:58 +0100

    Test completed successfully

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    let min = 0;
+    let max = 0;
+    let i = 0;
+    let sum = 0;
+    if(num1 > num2){
+        max = num1;
+        min = num2;
+    }else{
+        max = num2;
+        min = num1;
+    }
+    if(typeof(num1) != "number" || typeof(num2) != "number" || num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    for(i = min; i <= max; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
