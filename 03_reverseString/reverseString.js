3b821d0a02bf20b54f8a7ba4385f1342460a60cc
GianmarcoMorelli <gianmarco.morelli96@gmail.com>
2023-01-18 13:47:33 +0100

    Test passed succesfully

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let result = "";
+    let resultTmp = word.split("");
+    let len = word.length;
+    let i = 0;
+    let resultTmpTwo = [];
+    console.log(resultTmp[i]);
+    for(i = 0; i < len; i++){
+        resultTmpTwo.splice(0,0,resultTmp[i]);
+    }
+    console.log(resultTmp);
+    console.log(resultTmpTwo);
+    result = resultTmpTwo.join("");
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
