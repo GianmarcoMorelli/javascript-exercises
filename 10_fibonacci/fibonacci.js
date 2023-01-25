52f132fd886f1f4e8ad8d35efbb276791efaa488
GianmarcoMorelli <gianmarco.morelli96@gmail.com>
2023-01-25 10:46:05 +0100

    Test Completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(inputNum) {
+    let i = 0;
+    let sequenceArr = [];
+    if(inputNum > 0){
+        for(i = 0; i < inputNum; i++){
+            if(i == 0 || i == 1){
+                sequenceArr[i] = 1;
+            }else{
+                sequenceArr[i] = sequenceArr[i - 1] + sequenceArr[i - 2];
+            }
+        }
+        return sequenceArr[inputNum - 1];
+    }else{
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
