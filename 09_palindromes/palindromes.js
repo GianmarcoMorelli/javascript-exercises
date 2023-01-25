a506a24efb499e17e9ebc7bd88401eb7ee8d041b
GianmarcoMorelli <gianmarco.morelli96@gmail.com>
2023-01-25 10:26:42 +0100

    Test completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+    let stringNoSpecialChar = "";
+    stringNoSpecialChar = inputString.replace(/[^a-zA-Z0-9 ]/g,"");
+    let i = 0;
+    let res = "";
+    let stringNoSpaces = stringNoSpecialChar.replace(/\s+/g,"");
+    let len = stringNoSpaces.length;
+    for(i = len-1; i >= 0; i--){
+        res += stringNoSpaces[i];
+    }
 
+    if(res.toUpperCase() == stringNoSpaces.toUpperCase()){
+        return true;
+    }else{
+        return false;
+    }
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
