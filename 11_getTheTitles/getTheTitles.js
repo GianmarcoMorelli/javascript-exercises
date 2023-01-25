22220c215d32caf9efe095506677da2431ac67ae
GianmarcoMorelli <gianmarco.morelli96@gmail.com>
2023-01-25 11:25:21 +0100

    Test Completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(inputArray) {
+    let len = inputArray.length;
+    let res = [];
+    let i = 0;
+    for(i = 0; i < len; i ++){
+        res.push(inputArray[i].title);
+    }
+    return res;
 };
 
 // Do not edit below this line
